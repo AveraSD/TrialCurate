84d7a2b5af5bdd107b7f042afb39251ae8e02e1b
shivUSF <30928989+shivUSF@users.noreply.github.com>
2023-04-26 18:22:19 -0500

    added all the required files first pass

---
+++ R/add_trials.R
@@ -0,0 +1,23 @@
+library(data.table)
+library(DT)
+library(dplyr)
+# function to write trial.ndjson after final Submit button is clicked
+
+outSubmit <- function() {
+  
+  tr2 <- isolate(disAd$rsdf)
+  
+  # Connect to the MongoDB server
+  mongo <- mongo(db = "aci", 
+                 collection = "ClinicalTrials", 
+                 url = "mongodb://127.0.0.1:27017")
+  outjson <- paste0(here(trial_data_dir), 
+                    paste0(tr2 %>% unnest(c(info, disease, query)) %>% select(NCT) %>% as.character(), ".full.ndjson"))
+  writeLines(tr2 %>% toJSON(pretty = T), outjson)
+  
+  json_data_file <- do.call(rbind,lapply(paste(readLines(outjson, warn=FALSE),
+                                         collapse=""),jsonlite::fromJSON))
+  mongo$insert(as.data.frame(json_data_file))
+ # message(paste0("Written to file: ", outjson))
+}
+
---
+++ ui.R
@@ -0,0 +1,72 @@
+#
+# This is the user-interface definition of a TrialCurate application. You can
+# run the application by clicking 'Run App' above.
+#
+
+# Required library
+library(shiny)
+library(bslib)
+library(mongolite)
+library(jsonlite)
+library(httr)
+library(tidyverse)
+library(shinyWidgets)
+library(shinyFiles)
+library(DT)
+library(shinyjs)
+library(config)
+library(data.table)
+library(dplyr)
+library(here)
+
+# sourced the files 
+source(here("R", "curateUI.R"))
+source(here("R", "curateServer.R"))
+
+
+# Define UI for application that draws a histogram
+ui <- tagList(
+  tags$head(
+    tags$link(rel = "stylesheet", type = "text/css", href = "style.css")
+  ), 
+  useShinyjs(),
+  navbarPage(
+    title = "TrialCurate", 
+    
+    tabPanel("Add Trial",
+             
+             tabsetPanel(
+               id = 'inNav',
+               
+               # Curate Query
+               tabPanel("NCT ID", 
+                        input_form),
+               
+               # Disease 
+               tabPanel("Disease",
+                        dise_form),
+               
+               # Biomarker 
+               tabPanel("Biomarker",
+                        biom_form),
+               
+               # Documentation
+               tabPanel("Documents",
+                        doc_form),
+               
+               # View trial before submitting to database
+               tabPanel("View Trial",
+                        dis_form )
+               
+             )),
+    # tabPanel(
+    #   "Browse Trials"
+    # ),
+    
+    theme = bs_theme(version = 5, 
+                     bootswatch = "cosmo",
+                     primary = "#246725")
+    
+  )
+)
+
